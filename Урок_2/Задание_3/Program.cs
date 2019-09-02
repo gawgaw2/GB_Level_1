b1cfd244593c2fc2d36b6c6fae816618e197a1d1
gawgaw2 <2321554@mail.ru>
2019-09-03 01:21:11 +0300

    Урок_2

---
+++ Урок_2/Задание_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //С клавиатуры вводятся числа, пока не будет введен 0.
+            //Подсчитать сумму всех нечетных положительных чисел.
+            int count = 0;
+            string mes = "";
+            int intMes = 0;
+
+            do
+            {
+                Console.WriteLine("Введите число");
+                mes=Console.ReadLine();
+                intMes = int.Parse(mes);
+                if (intMes > 0 && (intMes % 2 != 0)) count++;
+              
+            } while (intMes != 0);
+            Console.WriteLine("Количество положительных и нечетных чисел = " + count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Урок_2/Задание_4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Реализовать метод проверки логина и пароля. 
+            //На вход подается логин и пароль. 
+            //На выходе истина, если прошел авторизацию, и ложь, 
+            //если не прошел (Логин: root, Password: GeekBrains). 
+            //Используя метод проверки логина и пароля, 
+            //написать программу: пользователь вводит логин и пароль, 
+            //программа пропускает его дальше или не пропускает.
+            //С помощью цикла do while ограничить ввод пароля тремя попытками.
+            string log = "";
+            string pass = "";
+            int cou = 0;
+
+            do
+            {
+                Console.WriteLine("Введите логин");
+                log = Console.ReadLine();
+                Console.WriteLine("Введите пароль");
+                pass = Console.ReadLine();
+                if (verifi(log,pass))
+                {
+                    Console.WriteLine("Вы вошли!");
+                    break;
+                }else
+                {
+                    Console.WriteLine("Введны неправильные логин или пароль");
+                }
+                cou++;
+            } while (cou < 3);
+
+            Console.ReadKey();
+        }
+
+
+       static bool verifi(string log, string pass)
+        {
+            string login = "root";
+            string password = "GeekBrains";
+            bool res = false;
+
+            if (login == log && password == pass) 
+            {
+                res=true;
+            }else
+            {
+                res=false;
+            };
+            return res;
+        }
+    }
+}
